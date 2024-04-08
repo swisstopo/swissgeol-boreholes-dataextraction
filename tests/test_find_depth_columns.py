2f4c2c0b0f2de4b2dfe437861b1ad1ad12cb691b
Renato Durrer <renato.durrer@visium.ch>
2024-04-08 10:52:45 +0200

    Add tests for find_depth_columns

--- tests/test_find_depth_columns.py
+++ tests/test_find_depth_columns.py
@@ -2,7 +2,8 @@
 
 import fitz
 import pytest
-from stratigraphy.util.find_depth_columns import depth_column_entries
+from stratigraphy.util.depthcolumnentry import DepthColumnEntry
+from stratigraphy.util.find_depth_columns import depth_column_entries, find_depth_columns
 from stratigraphy.util.line import DepthInterval, TextLine
 
 
@@ -32,3 +33,46 @@ def test_depth_column_entries_with_splits():  # noqa: D103
     assert entries[1].value == 20.0, "The second entry should have a value of 20.0"
     assert entries[2].value == 30.0, "The third entry should have a value of 30.0"
     assert entries[3].value == 40.0, "The fourth entry should have a value of 40.0"
+
+
+all_words_find_depth_column = [
+    TextLine([DepthInterval(fitz.Rect(0, 0, 5, 1), "10.00m")]),
+    TextLine([DepthInterval(fitz.Rect(20, 0, 30, 1), "Kies, Torf und Sand")]),
+    TextLine([DepthInterval(fitz.Rect(20, 2, 30, 3), "Kies, verwittert.")]),
+    TextLine([DepthInterval(fitz.Rect(0, 2, 5, 3), "20.0m")]),
+    TextLine([DepthInterval(fitz.Rect(0, 4, 5, 5), "30.0m")]),
+    TextLine([DepthInterval(fitz.Rect(0, 6, 5, 7), "40.0m")]),
+    TextLine([DepthInterval(fitz.Rect(0, 8, 5, 9), "50.0m")]),
+]
+
+
+def test_find_depth_columns_arithmetic_progression():  # noqa: D103
+    entries = [
+        DepthColumnEntry(fitz.Rect(0, 0, 5, 1), 10.0),
+        DepthColumnEntry(fitz.Rect(0, 2, 5, 3), 20.0),
+        DepthColumnEntry(fitz.Rect(0, 4, 5, 5), 30.0),
+        DepthColumnEntry(fitz.Rect(0, 6, 5, 7), 40.0),
+        DepthColumnEntry(fitz.Rect(0, 8, 5, 9), 50.0),
+    ]
+
+    columns = find_depth_columns(entries, all_words_find_depth_column)
+    assert len(columns) == 0, "There should be 0 columns as the above is a perfect arithmetic progression"
+
+
+def test_find_depth_columns():  # noqa: D103
+    entries = [
+        DepthColumnEntry(fitz.Rect(0, 0, 5, 1), 12.0),
+        DepthColumnEntry(fitz.Rect(0, 2, 5, 3), 20.0),
+        DepthColumnEntry(fitz.Rect(0, 4, 5, 5), 34.0),
+        DepthColumnEntry(fitz.Rect(0, 6, 5, 7), 40.0),
+        DepthColumnEntry(fitz.Rect(0, 8, 5, 9), 50.0),
+    ]
+
+    columns = find_depth_columns(entries, all_words_find_depth_column)
+    assert len(columns) == 1, "There should be 1 column"
+    assert len(columns[0].entries) == 5, "The column should have 4 entries"
+    assert pytest.approx(columns[0].entries[0].value) == 12.0, "The first entry should have a value of 12.0"
+    assert pytest.approx(columns[0].entries[1].value) == 20.0, "The second entry should have a value of 20.0"
+    assert pytest.approx(columns[0].entries[2].value) == 34.0, "The third entry should have a value of 34.0"
+    assert pytest.approx(columns[0].entries[3].value) == 40.0, "The fourth entry should have a value of 40.0"
+    assert pytest.approx(columns[0].entries[4].value) == 50.0, "The fourth entry should have a value of 50.0"
