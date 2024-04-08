b91c6a97051e58516706349929b3c47c42025f2a
Renato Durrer <renato.durrer@visium.ch>
2024-04-08 11:31:22 +0200

    Added test case with two columns for find_layer_depth_columns

--- tests/test_find_depth_columns.py
+++ tests/test_find_depth_columns.py
@@ -136,3 +136,33 @@ def test_find_layer_depth_columns():  # noqa: D103
     assert columns[0].entries[3].end.value == 50.0, "The fourth entry should have a value of 50.0"
     assert columns[0].entries[4].start.value == 50.0, "The fourth entry should have a value of 50.0"
     assert columns[0].entries[4].end.value == 60.0, "The fourth entry should have a value of 60.0"
+
+
+def test_two_columns_find_layer_depth_columns():  # noqa: D103
+    entries = [  # first depth column
+        DepthColumnEntry(fitz.Rect(0, 0, 5, 1), 12.0),  # layer 12.0-20.0m
+        DepthColumnEntry(fitz.Rect(0, 0, 5, 1), 20.0),
+        DepthColumnEntry(fitz.Rect(0, 2, 5, 3), 20.0),  # layer 20.0-34.0m
+        DepthColumnEntry(fitz.Rect(0, 2, 5, 3), 34.0),
+        DepthColumnEntry(fitz.Rect(0, 4, 5, 5), 34.0),  # layer 34.0-40.0m
+        DepthColumnEntry(fitz.Rect(0, 4, 5, 5), 40.0),
+        DepthColumnEntry(fitz.Rect(0, 6, 5, 7), 40.0),  # layer 40.0-50.0m
+        DepthColumnEntry(fitz.Rect(0, 6, 5, 7), 50.0),
+        DepthColumnEntry(fitz.Rect(0, 8, 5, 9), 50.0),  # layer 50.0-60.0m
+        DepthColumnEntry(fitz.Rect(0, 8, 5, 9), 60.0),
+        # second depth column
+        DepthColumnEntry(fitz.Rect(20, 0, 25, 1), 12.0),  # layer 12.0-20.0m
+        DepthColumnEntry(fitz.Rect(20, 0, 25, 1), 20.0),
+        DepthColumnEntry(fitz.Rect(20, 2, 25, 3), 20.0),  # layer 20.0-34.0m
+        DepthColumnEntry(fitz.Rect(20, 2, 25, 3), 34.0),
+        DepthColumnEntry(fitz.Rect(20, 4, 25, 5), 34.0),  # layer 34.0-40.0m
+        DepthColumnEntry(fitz.Rect(20, 4, 25, 5), 40.0),
+        DepthColumnEntry(fitz.Rect(20, 6, 25, 7), 40.0),  # layer 40.0-50.0m
+        DepthColumnEntry(fitz.Rect(20, 6, 25, 7), 50.0),
+        DepthColumnEntry(fitz.Rect(20, 8, 25, 9), 50.0),  # layer 50.0-60.0m
+        DepthColumnEntry(fitz.Rect(20, 8, 25, 9), 60.0),
+    ]
+    columns = find_layer_depth_columns(entries, all_words_find_layer_depth_column)
+    assert len(columns) == 2, "There should be 2 columns"
+    assert len(columns[0].entries) == 5, "The first column should have 5 entries"
+    assert len(columns[1].entries) == 5, "The second column should have 5 entries"
