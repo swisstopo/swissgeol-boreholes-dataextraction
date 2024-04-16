a3d233e1c8a4252f73e34889a0e49c6b0123f600
Renato Durrer <renato.durrer@visium.ch>
2024-04-16 16:30:14 +0200

    Deal with None value for datapath

--- src/stratigraphy/__init__.py
+++ src/stratigraphy/__init__.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATAPATH = Path(os.getenv("BOREHOLES_DATA_PATH"))
+if os.getenv("BOREHOLES_DATA_PATH") is not None:
+    DATAPATH = Path(os.getenv("BOREHOLES_DATA_PATH"))
+else:
+    DATAPATH = Path(__file__).parent.parent.parent / "data"
 
 PROJECT_ROOT = Path(os.path.abspath(__file__)).parent.parent.parent
