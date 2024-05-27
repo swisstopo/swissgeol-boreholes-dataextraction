459c5d963dcb1fc20c9207c373900f1ccc6d60b4
Renato Durrer <renato.durrer@visium.ch>
2024-05-27 09:59:12 +0200

    Improve duplicate detection to use depth information

--- src/stratigraphy/main.py
+++ src/stratigraphy/main.py
@@ -183,7 +183,8 @@ def start_pipeline(
                         if page_index > 0:
                             layer_predictions = remove_duplicate_layers(
                                 doc[page_index - 1],
-                                page,
+                                doc[page_index],
+                                predictions[filename][f"page_{page_number - 1}"]["layers"],
                                 layer_predictions,
                                 matching_params["img_template_probability_threshold"],
                             )
