94cd7d315881c40fead559b4dffb9230ab4fa5f5
lillemor <lillemor.haibach@swisstopo.ch>
2025-01-14 15:24:37 +0100

    numeric_columns to clusters

--- src/stratigraphy/sidebar/a_above_b_sidebar_extractor.py
+++ src/stratigraphy/sidebar/a_above_b_sidebar_extractor.py
@@ -1,5 +1,8 @@
 """Module for finding AAboveBSidebar instances in a borehole profile."""
 
+import logging
+from collections import defaultdict
+
 import fitz
 
 from stratigraphy.depth import DepthColumnEntryExtractor
@@ -8,55 +11,53 @@ from stratigraphy.lines.line import TextWord
 from .a_above_b_sidebar import AAboveBSidebar
 from .a_above_b_sidebar_validator import AAboveBSidebarValidator
 
+logger = logging.getLogger(__name__)
+
 
 class AAboveBSidebarExtractor:
     """Class that finds AAboveBSidebar instances in a borehole profile."""
 
     @staticmethod
-    def find_in_words(
+    def find_in_words(  ## help find correct sidebar based on vertical geometric lines!
         all_words: list[TextWord], used_entry_rects: list[fitz.Rect], sidebar_params: dict
     ) -> list[AAboveBSidebar]:
         """Construct all possible AAboveBSidebar objects from the given words.
 
         Args:
-            all_words (list[TextLine]): All words in the page.
+            all_words (list[TextWord]): All words in the page.
             used_entry_rects (list[fitz.Rect]): Part of the document to ignore.
             sidebar_params (dict): Parameters for the AAboveBSidebar objects.
 
         Returns:
             list[AAboveBSidebar]: Found AAboveBSidebar objects.
         """
-        entries = [
+        entries = [  ##takes sometime
             entry
             for entry in DepthColumnEntryExtractor.find_in_words(all_words, include_splits=False)
             if entry.rect not in used_entry_rects
         ]
+        logger.info("entries %s", entries)
 
-        numeric_columns: list[AAboveBSidebar] = []
+        clusters = defaultdict(lambda: AAboveBSidebar(entries=[]))
         for entry in entries:
-            has_match = False
-            additional_columns = []
-            for column in numeric_columns:
-                if column.can_be_appended(entry.rect):
-                    has_match = True
-                    column.entries.append(entry)
-                else:
-                    valid_initial_segment = column.valid_initial_segment(entry.rect)
-                    if len(valid_initial_segment.entries) > 0:
-                        has_match = True
-                        valid_initial_segment.entries.append(entry)
-                        additional_columns.append(valid_initial_segment)
+            x0 = entry.rect.x0
+            matched_cluster = []
 
-            numeric_columns.extend(additional_columns)
-            if not has_match:
-                numeric_columns.append(AAboveBSidebar(entries=[entry]))
+            for cluster_x0 in clusters:
+                if abs(cluster_x0 - x0) <= 10:  ##TODO: edge cases + overlaps handling should be implemented,
+                    # set the threshold based on page or entry dimensions?
+                    # -> done by allowing entry to belong to multiple clusters ->
+                    # use bisect? to reduce time?
+                    matched_cluster.append(cluster_x0)
 
-            # only keep columns that are not contained in a different column
-            numeric_columns = [
-                column
-                for column in numeric_columns
-                if all(not other.strictly_contains(column) for other in numeric_columns)
-            ]
+            if matched_cluster:
+                for cluster_x0 in matched_cluster:
+                    clusters[cluster_x0].entries.append(entry)
+            else:
+                clusters[x0].entries.append(entry)
+        numeric_columns = list(clusters.values())
+
+        logger.info("numeric clusters %s", numeric_columns)
 
         sidebar_validator = AAboveBSidebarValidator(all_words, **sidebar_params)
 
@@ -69,6 +70,16 @@ class AAboveBSidebarExtractor:
             if not column.significant_arithmetic_progression()
         ]
 
+        logger.info("numeric_columns valid %s", numeric_columns)
+
+        for column in numeric_columns:  ## create helper function
+            if column:
+                integer_entries = [entry for entry in column.entries if isinstance(entry.value, int)]
+                if integer_entries:
+                    integer_subset = AAboveBSidebar(integer_entries)
+                    if integer_subset.significant_arithmetic_progression():
+                        column.entries = [entry for entry in column.entries if entry not in integer_entries]
+        logger.info("numeric_columns filtered out integers%s", numeric_columns)
         return sorted(
             [column for column in numeric_columns if column and sidebar_validator.is_valid(column)],
             key=lambda column: len(column.entries),
