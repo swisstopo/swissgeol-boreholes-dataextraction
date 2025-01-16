da8dd5e68f60b147ca53a9bd1ba05202a0c658f4
lillemor <lillemor.haibach@swisstopo.ch>
2025-01-16 15:09:05 +0100

    parse string to float without correcting

--- src/stratigraphy/depth/util.py
+++ src/stratigraphy/depth/util.py
@@ -1,10 +1,7 @@
 """Contains utility functions for depth column entries."""
 
-import re
-
 
 def value_as_float(string_value: str) -> float:  # noqa: D103
     """Converts a string to a float."""
     # OCR sometimes tends to miss the decimal comma
-    parsed_text = re.sub(r"^-?([0-9]+)([0-9]{2})", r"\1.\2", string_value)
-    return abs(float(parsed_text))
+    return abs(float(string_value))
