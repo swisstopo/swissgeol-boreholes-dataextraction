13afebc43c183f7f3f0f1ae71fc779e11a00b34e
lillemor <lillemor.haibach@swisstopo.ch>
2025-01-16 11:23:26 +0100

    take regex outside of loop

--- src/stratigraphy/depth/depthcolumnentry_extractor.py
+++ src/stratigraphy/depth/depthcolumnentry_extractor.py
@@ -26,10 +26,11 @@ class DepthColumnEntryExtractor:
             list[DepthColumnEntry]: The extracted depth column entries.
         """
         entries = []
+        regex = re.compile(r"^-?\.?([0-9]+(\.[0-9]+)?)[müMN\\.]*$")
+
         for word in sorted(all_words, key=lambda word: word.rect.y0):
             try:
                 input_string = word.text.strip().replace(",", ".")
-                regex = re.compile(r"^-?\.?([0-9]+(\.[0-9]+)?)[müMN\\.]*$")
                 # numbers such as '.40' are not supported. The reason is that sometimes the OCR
                 # recognizes a '-' as a '.' and we just ommit the leading '.' to avoid this issue.
                 match = regex.match(input_string)
