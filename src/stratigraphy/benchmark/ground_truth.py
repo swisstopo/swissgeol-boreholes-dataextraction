ac626d6b2e20b48d46d70b3af2b25df3c0af4ca8
TicaGit <thibaud.trinca@visium.ch>
2025-03-07 12:51:42 +0100

    modify how groundtruth object is loaded

--- src/stratigraphy/benchmark/ground_truth.py
+++ src/stratigraphy/benchmark/ground_truth.py
@@ -14,25 +14,32 @@ class GroundTruth:
     """Ground truth data for the stratigraphy benchmark."""
 
     def __init__(self, path: Path) -> None:
-        self.ground_truth = defaultdict(dict)
+        """Instanciate the GroundTruth object.
+
+        Args:
+            path (Path): the path to the Ground truth file
+        """
+        self.ground_truth = defaultdict(lambda: defaultdict(dict))
 
         # Load the ground truth data
         with open(path, encoding="utf-8") as in_file:
             ground_truth = json.load(in_file)
 
         # Parse the ground truth data
-        for borehole_profile, ground_truth_item in ground_truth.items():
-            layers = ground_truth_item["layers"]
-            self.ground_truth[borehole_profile]["layers"] = [
-                {
-                    "material_description": parse_text(layer["material_description"]),
-                    "depth_interval": layer["depth_interval"],
-                }
-                for layer in layers
-                if parse_text(layer["material_description"]) != ""
-            ]
-            self.ground_truth[borehole_profile]["metadata"] = ground_truth_item["metadata"]
-            self.ground_truth[borehole_profile]["groundwater"] = ground_truth_item["groundwater"]
+        for file_name, ground_truth_item in ground_truth.items():
+            for borehole_data in ground_truth_item:
+                layers = borehole_data["layers"]
+                borehole_index = borehole_data["borehole_index"]
+                self.ground_truth[file_name][borehole_index]["layers"] = [
+                    {
+                        "material_description": parse_text(layer["material_description"]),
+                        "depth_interval": layer["depth_interval"],
+                    }
+                    for layer in layers
+                    if parse_text(layer["material_description"]) != ""
+                ]
+                self.ground_truth[file_name][borehole_index]["metadata"] = borehole_data["metadata"]
+                self.ground_truth[file_name][borehole_index]["groundwater"] = borehole_data["groundwater"]
 
     def for_file(self, file_name: str) -> dict:
         """Get the ground truth data for a given file.
