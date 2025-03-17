3cfd14dafb5bb70fbaa33da597970634659cb094
TicaGit <thibaud.trinca@visium.ch>
2025-03-17 11:29:14 +0100

    add one test for prediction to ground truth matching (with >1 borehole in pdf)

--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -7,11 +7,13 @@ import fitz
 import pytest
 from stratigraphy.benchmark.ground_truth import GroundTruth
 from stratigraphy.data_extractor.data_extractor import FeatureOnPage
+from stratigraphy.evaluation.layer_evaluator import LayerEvaluator
 from stratigraphy.evaluation.utility import count_against_ground_truth
 from stratigraphy.groundwater.groundwater_extraction import Groundwater, GroundwatersInBorehole
-from stratigraphy.layer.layer import LayersInBorehole
+from stratigraphy.layer.layer import Layer, LayerDepths, LayerDepthsEntry, LayersInBorehole
 from stratigraphy.metadata.coordinate_extraction import CoordinateEntry, LV95Coordinate
 from stratigraphy.metadata.metadata import BoreholeMetadata, FileMetadata
+from stratigraphy.text.textblock import MaterialDescription
 from stratigraphy.util.borehole_predictions import BoreholePredictionsWithGroundTruth, FilePredictionsWithGroundTruth
 from stratigraphy.util.file_predictions import FilePredictions
 from stratigraphy.util.overall_file_predictions import OverallFilePredictions
@@ -66,9 +68,88 @@ def sample_file_prediction() -> FilePredictions:
 
 
 @pytest.fixture
+def file_prediction_with_two_boreholes() -> FilePredictions:
+    """Fixture to create a sample FilePredictions object that has two boreholes."""
+    filename = "example_borehole_profile.pdf"
+    coord = FeatureOnPage(
+        feature=LV95Coordinate(
+            east=CoordinateEntry(coordinate_value=2789456), north=CoordinateEntry(coordinate_value=1123012)
+        ),
+        rect=fitz.Rect(),
+        page=1,
+    )
+
+    layers_in_borehole = LayersInBorehole(
+        [
+            Layer(
+                material_description=FeatureOnPage(MaterialDescription(text=descr, lines=[]), fitz.Rect(), 0),
+                depths=LayerDepths(LayerDepthsEntry(start, fitz.Rect()), LayerDepthsEntry(end, fitz.Rect())),
+            )
+            for descr, start, end in [
+                ("HUMUS", None, 1),
+                ("KIES, grau", 1, 2),
+                ("sand", 2, 3),
+            ]
+        ]
+    )
+    layers_in_borehole_2 = LayersInBorehole(
+        [
+            Layer(
+                material_description=FeatureOnPage(MaterialDescription(text=descr, lines=[]), fitz.Rect(), 0),
+                depths=LayerDepths(LayerDepthsEntry(start, fitz.Rect()), LayerDepthsEntry(end, fitz.Rect())),
+            )
+            for descr, start, end in [
+                ("KIES, Sand,", 0.0, 0.5),
+                ("stein, sand", 0.5, 2.0),
+            ]
+        ]
+    )
+
+    dt_date = datetime(2024, 10, 1)
+    groundwater_on_page = FeatureOnPage(
+        feature=Groundwater(depth=100, date=dt_date, elevation=20),
+        page=1,
+        rect=fitz.Rect(0, 0, 100, 100),
+    )
+    groundwater_in_bh = GroundwatersInBorehole(groundwater_feature_list=[groundwater_on_page])
+
+    file_metadata = FileMetadata(language="en", filename=filename, page_dimensions=[Mock(width=10, height=20)])
+    metadata = BoreholeMetadata(coordinates=coord, elevation=None)
+
+    return FilePredictions(
+        [
+            BoreholePredictions(
+                borehole_index=0,
+                layers_in_borehole=layers_in_borehole,
+                file_name=filename,
+                metadata=metadata,
+                groundwater_in_borehole=groundwater_in_bh,
+                bounding_boxes=[],
+            ),
+            BoreholePredictions(
+                borehole_index=1,
+                layers_in_borehole=layers_in_borehole_2,
+                file_name=filename,
+                metadata=metadata,
+                groundwater_in_borehole=groundwater_in_bh,
+                bounding_boxes=[],
+            ),
+        ],
+        file_name=filename,
+        file_metadata=file_metadata,
+    )
+
+
+@pytest.fixture
 def groundtruth():
     """Path to the ground truth file."""
     return GroundTruth("example/example_groundtruth.json")
+
+
+@pytest.fixture
+def groundtruth_with_two_boreholes():
+    """Path to the ground truth file that has two boreholes."""
+    return GroundTruth("example/example_layers_groundtruth.json")
 
 
 @pytest.fixture
@@ -119,14 +200,27 @@ def test_overall_file_predictions(sample_file_prediction: FilePredictions):
     assert set(result.keys()) == {"example_borehole_profile.pdf"}
 
 
+def test_evaluate_layer_matching(
+    file_prediction_with_two_boreholes: FilePredictions, groundtruth_with_two_boreholes: GroundTruth
+):
+    """Test the matching of predictions to ground truths when multiple boreholes are present in one document."""
+    groundtruth_for_file = groundtruth_with_two_boreholes.for_file("example_borehole_profile.pdf")
+    sample_file_prediction_with_ground_truth: FilePredictionsWithGroundTruth = (
+        LayerEvaluator.match_predictions_with_ground_truth(file_prediction_with_two_boreholes, groundtruth_for_file)
+    )
+    # We test the matching by comparing the number of layers, one borehole has 2, the other has 3.
+    assert all(
+        [
+            len(pred.predictions.layers_in_borehole.layers) == len(pred.ground_truth["layers"])
+            for pred in sample_file_prediction_with_ground_truth
+        ]
+    )
+
+
 def test_evaluate_metadata_extraction(sample_file_prediction_with_ground_truth: FilePredictionsWithGroundTruth):
     """Test evaluate_metadata_extraction method of OverallFilePredictions."""
-    overall_predictions = OverallFilePredictions()
-    overall_predictions.add_file_predictions(sample_file_prediction)
-    all_predsk_with_gt = AllBoreholePredictionsWithGroundTruth([sample_file_prediction_with_ground_truth])
-
-    #
-    metadata_metrics = all_predsk_with_gt.evaluate_metadata_extraction()
+    all_predictions_with_gt = AllBoreholePredictionsWithGroundTruth([sample_file_prediction_with_ground_truth])
+    metadata_metrics = all_predictions_with_gt.evaluate_metadata_extraction()
 
     assert metadata_metrics is not None  # Ensure the evaluation returns a result
 
