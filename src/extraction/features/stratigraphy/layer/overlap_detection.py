ba3b24c530d2ce66fa2aae8ad84a57cc39a14f59
TicaGit <thibaud.trinca@visium.ch>
2025-09-01 13:14:01 +0200

    adress comments

--- src/extraction/features/stratigraphy/layer/overlap_detection.py
+++ src/extraction/features/stratigraphy/layer/overlap_detection.py
@@ -4,9 +4,14 @@ import logging
 
 import Levenshtein
 
+from utils.file_utils import read_params
+
 from .layer import ExtractedBorehole, Layer
 
 logger = logging.getLogger(__name__)
+
+
+matching_params = read_params("matching_params.yml")  # add the duplicate tresh to the params 0.95
 
 
 def remove_scan_overlap_layers(
@@ -39,20 +44,17 @@ def remove_scan_overlap_layers(
         assert (
             len({page for layer in current_borehole.predictions for page in layer.material_description.pages}) == 1
         ), "At this point, all layers should be on the same page."
-        sorted_layers = sorted(
-            current_borehole.predictions, key=lambda x: x.material_description.rects_with_pages[0].rect.y0
-        )
+        bottom_duplicate_idx = find_last_duplicate_layer_index(previous_page_layers, current_borehole.predictions)
 
-        bottom_duplicate_idx = find_last_duplicate_layer_index(previous_page_layers, sorted_layers)
-
-        non_duplicated_extracted_boreholes.append(
-            ExtractedBorehole(
-                predictions=sorted_layers[bottom_duplicate_idx + 1 :]
-                if bottom_duplicate_idx is not None
-                else sorted_layers,
-                bounding_boxes=current_borehole.bounding_boxes,
+        if bottom_duplicate_idx is not None:
+            non_duplicated_extracted_boreholes.append(
+                ExtractedBorehole(
+                    predictions=current_borehole.predictions[bottom_duplicate_idx + 1 :],
+                    bounding_boxes=current_borehole.bounding_boxes,
+                )
             )
-        )
+        else:
+            non_duplicated_extracted_boreholes.append(current_borehole)
     return non_duplicated_extracted_boreholes
 
 
@@ -68,45 +70,39 @@ def find_last_duplicate_layer_index(previous_page_layers: list[Layer], sorted_la
     Returns:
         int | None: Index of the last duplicate layer or None if not found
     """
-    compare_against_idx = -1  # begin comparison against the last of previous page
+    compare_against_idx = len(previous_page_layers) - 1  # begin comparison against the last of previous page
     layer_idx = len(sorted_layers) - 1  # begin from the bottom of the current page, then validate upwards
 
     bottom_duplicate_idx = None
+    t = matching_params["duplicate_layer_threshold"]
 
     while layer_idx >= 0:
         current_layer = sorted_layers[layer_idx]
         if not current_layer.material_description:
             layer_idx -= 1
             continue
-        if abs(compare_against_idx) > len(previous_page_layers):
+        if compare_against_idx < 0:
             break  # we've run out of previous layers to compare against
 
         previous_page_layer = previous_page_layers[compare_against_idx]
 
         # 1. check of the current layer with the compare_against_idx'th layer of previous page.
-        if _is_duplicate(current_layer.material_description.text, previous_page_layer.material_description.text):
+        if _is_duplicate(current_layer.material_description.text, previous_page_layer.material_description.text, t):
             # 2. check in case of wrong lines split of the merged layer with the compare_against_idx'th layer of
             # previous page.
-            text_merged = ""
+            layer_idx_delta = 1
             if layer_idx >= 1 and sorted_layers[layer_idx - 1].material_description:
-                text_merged = (
-                    sorted_layers[layer_idx - 1].material_description.text
-                    + " "
-                    + current_layer.material_description.text
+                text_merged = " ".join(
+                    [sorted_layers[layer_idx - 1].material_description.text, current_layer.material_description.text]
                 )
-            if _is_duplicate(text_merged, previous_page_layer.material_description.text):
-                # If the merged is also a duplicate, we should skip both layers.
-                if bottom_duplicate_idx is None:
-                    # record the lowest layer that is duplicated (i.e. the first encountered, as we iterate backwards).
-                    bottom_duplicate_idx = layer_idx
-                layer_idx -= 2  # skip the wrongly split layer
-                compare_against_idx -= 1
-                continue
+                if _is_duplicate(text_merged, previous_page_layer.material_description.text, t):
+                    # If the merged is also a duplicate, we should skip both layers.
+                    layer_idx_delta = 2
 
-            # If the merge is not a duplicated, this is the standard case, and we continue verifying the layers above.
             if bottom_duplicate_idx is None:
+                # record the lowest layer that is duplicated (i.e. the first encountered, as we iterate backwards).
                 bottom_duplicate_idx = layer_idx
-            layer_idx -= 1
+            layer_idx -= layer_idx_delta  # skip the wrongly split layer(s)
             compare_against_idx -= 1
             continue
 
@@ -118,12 +114,12 @@ def find_last_duplicate_layer_index(previous_page_layers: list[Layer], sorted_la
         # was a false positive and we reset the search
         layer_idx = bottom_duplicate_idx - 1  # just above the false positive
         bottom_duplicate_idx = None
-        compare_against_idx = -1  # reset with the last layer of the previous page
+        compare_against_idx = len(previous_page_layers) - 1  # reset with the last layer of the previous page
 
     return bottom_duplicate_idx
 
 
-def _is_duplicate(cur_text: str, prev_text: str, t=0.95):
+def _is_duplicate(cur_text: str, prev_text: str, t: float):
     """Detect if layer and prev_layer are duplicates across a page break.
 
     Strategy:
@@ -134,15 +130,15 @@ def _is_duplicate(cur_text: str, prev_text: str, t=0.95):
     Args:
         cur_text (str): The text of the current layer to compare.
         prev_text (str): The text of the previous layer to compare against.
-        t (float): The similarity threshold (default is 0.95).
+        t (float): The similarity threshold.
 
     Returns:
         bool: True if the layers are considered duplicates, False otherwise.
     """
     cur_text = cur_text.lower()
-    cur_words = cur_text.split()
     prev_text = prev_text.lower()
-    prev_words = prev_text.split()
-    return any(Levenshtein.ratio(cur_text, " ".join(prev_words[i:])) > t for i in range(len(prev_words))) or any(
-        Levenshtein.ratio(" ".join(cur_words[: i + 1]), prev_text) > t for i in range(len(cur_words))
+    min_length = min(len(cur_text), len(prev_text))
+    return (
+        Levenshtein.ratio(cur_text, prev_text[-min_length:]) > t
+        or Levenshtein.ratio(cur_text[:min_length], prev_text) > t
     )
