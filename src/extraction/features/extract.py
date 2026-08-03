fb19ae0383ed38c4c076c49e92e9e97b5eccb909
Stijn Vermeeren <stijn.vermeeren@swisstopo.ch>
2026-08-03 15:57:07 +0200

    change text extraction from PDF logic + add text_angle property to TextLine object

--- src/extraction/features/extract.py
+++ src/extraction/features/extract.py
@@ -380,7 +380,7 @@ class BoreholeExtractor:
         horizontal_text_lines = [
             line
             for line in self.lines
-            if line.rect.width > line.rect.height and not re.fullmatch(r"[\d\s.,\-/]+", line.text.strip())
+            if abs(line.text_angle) < 10 and not re.fullmatch(r"[\d\s.,\-/]+", line.text.strip())
         ]
         candidate_description = [line for line in horizontal_text_lines if check_y0_condition(line.rect.y0)]
 
